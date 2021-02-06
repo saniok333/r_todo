f4ef0c243ecd926fade01142c673df81811a9112
Oleksandr Olshevskyi <o.olshevskyi@viseven.com>
2021-02-06 22:29:09 +0200

    add item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,22 +4,46 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter'
+import ItemAddForm from '../item-add-form'
 
 import './app.css'
 
 export default class App extends Component {
 
+   todoDataStart = [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+   ]
+
+   maxId = this.todoDataStart.length + 1;
+
    state = {
       todoData: [
-         { label: 'Drink Coffee', important: false, id: 1 },
-         { label: 'Make Awesome App', important: true, id: 2 },
-         { label: 'Have a lunch', important: false, id: 3 }
+         ...this.todoDataStart
       ]
    }
 
    deleteItem1 = (idArg) => {
       this.setState(({ todoData }) => {
          const newArray = todoData.filter((el) => el.id !== idArg)
+
+         return {
+            todoData: newArray
+         }
+      })
+   }
+
+   addItem = (text) => {
+      // const newId = todoData[todoData.length - 1].id + 1;
+
+      const newId = this.maxId++;
+
+      const newItem = { label: text, important: false, id: newId };
+
+      this.setState(({ todoData }) => {
+
+         const newArray = [...todoData, newItem];
 
          return {
             todoData: newArray
@@ -43,6 +67,8 @@ export default class App extends Component {
             <TodoList
                todos={todoData}
                onDeleted1={this.deleteItem1} />
+            <ItemAddForm
+               onItemAdded={this.addItem} />
          </div>
       )
    }
---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css'
+
+export default class ItemAddForm extends Component {
+
+   render() {
+      const { onItemAdded } = this.props
+      return (
+         <div className="item-add-form">
+            <button
+               className="btn btn-outline-secondary"
+               onClick={() => onItemAdded('Hello Sasha')}>
+               Add Item
+               </button>
+         </div>
+      )
+   }
+
+
+
+}
+
